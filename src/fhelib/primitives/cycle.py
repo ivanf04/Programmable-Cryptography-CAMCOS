f00061482dc03255e8460da4c36e90c9b157303c
noperi <11732476+noperi@users.noreply.github.com>
2026-03-29 16:55:19 -0700

    update documentation, no code change

--- src/fhelib/primitives/cycle.py
+++ src/fhelib/primitives/cycle.py
@@ -4,6 +4,9 @@ Implemetation of cycle using np.roll
 if(k>0) elements move right
 if(k<0) elements move left
 
+Future: 
+    Can be modified to work with matrices + using axis parameter. 
+
 See documentation for examples:
 https://numpy.org/doc/stable/reference/generated/numpy.roll.html
 """

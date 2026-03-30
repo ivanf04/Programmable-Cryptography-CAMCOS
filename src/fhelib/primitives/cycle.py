5131bc8ab23964339c9c849d1a981f69280f1c47
noperi <11732476+noperi@users.noreply.github.com>
2026-03-29 21:35:21 -0700

    moved documentation to be consistent with other files

--- src/fhelib/primitives/cycle.py
+++ src/fhelib/primitives/cycle.py
@@ -1,17 +1,17 @@
+from fhelib import Ciphertext
+import numpy as np
+
 """"
 Implemetation of cycle using np.roll
 
 if(k>0) elements move right
 if(k<0) elements move left
 
-Future: 
+Future:
     Can be modified to work with matrices + using axis parameter. 
 
 See documentation for examples:
 https://numpy.org/doc/stable/reference/generated/numpy.roll.html
 """
-from fhelib import Ciphertext
-import numpy as np
-
 def cycle(ct: Ciphertext, k: int):
     return np.roll(ct, k)

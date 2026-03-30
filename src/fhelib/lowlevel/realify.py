984c8ff1ad549966fefe985bf76aa5440039687f
noperi <11732476+noperi@users.noreply.github.com>
2026-03-29 22:28:48 -0700

    add realify

---
+++ src/fhelib/lowlevel/realify.py
@@ -0,0 +1,12 @@
+from fhelib import Ciphertext
+#import numpy as np #unclear if needed
+
+""" convert ct to reals only from complex 
+    adding a complex number and its conjugate returns 2*(real),
+    so divide that by half
+    concrete FHE equivalent to np.real(ct)
+"""
+def realify(ct:Ciphertext):
+    return (ct + np.conjugate(ct)) * np.array(ptct, 0.5)
+
+# TODO by Dwyer: validate how we intend to call function using wrapper

8cd85dc11eee00226c6f993796f87c311c373089
ivanf04 <ifaranda2004@gmail.com>
2026-03-31 16:38:59 -0700

    fhelib-lowlevel-sign

--- src/fhelib/lowlevel/sign.py
+++ src/fhelib/lowlevel/sign.py
@@ -4,6 +4,7 @@ from fhelib import Ciphertext
 """
 Returns 0 if x_i is <= 0 or 1 if x_i > 0
 TODO: refactor with 'good_if'
+TODO: refactor to output {0,1/2,1}
 """
 
 """
@@ -21,17 +22,22 @@ sigmoid based approximation for the sign function
     out: Ciphertext
         Output with 1's and 0's 
 """
-def sign(x: Ciphertext, k=10.0, power=8, tol=1e-6) -> Ciphertext:
-    x = realify(x)  
+def sign(x: Ciphertext, k=10.0, tol=0.25) -> Ciphertext:
+    # x = realify(x)  
+    print(f"intial CT:\n{x}")
     s = 1.0 / (1.0 + np.exp(-k * x))
-    y = s ** power
 
-    # normalize values to 1 and zero based on tolerance
-    y[y <= tol] = 0.0   
-    y[y >= 1.0 - tol] = 1.0
+    # normalize values to 1, 1.5 and zero based on tolerance
+    s[s <= tol] = 0.0   
+    s[(s > tol) & (s < 1 - tol)] = 0.5
+    s[s >= 1.0 - tol] = 1.0
 
-    # clean up the data, set equal length CT to all 0 and 1 in corresponding elements of y 
-    out = np.zeros_like(y)
-    out[y > 0.5] = 1.0
-    return out
+    return s
 
+
+"""
+Use the original sign method to create a heaviside-type function
+see "CAMCOS 2026 Spring" hackmd for details 
+"""
+def sign_heaviside(x: Ciphertext, a, b, c) -> Ciphertext:
+    return a + ((b - a) * sign(x - c))
--- tests/sign_test.py
+++ tests/sign_test.py
@@ -1,15 +1,16 @@
 import numpy as np
 from fhelib import Ciphertext
-from fhelib.lowlevel.sign import sign
+from fhelib.lowlevel.sign import sign, sign_heaviside
+
 """
 test file for the sign function
 """
 
-a = Ciphertext(2 ** 15)
+a = Ciphertext(4)
 
 for i in range(a.size):
-    a[i] = i - 4
+    a[i] = i - (a.size / 2)
 
-print(f"intial CT:\n{a}")
 print(f"CT passed though sign:\n{sign(a)}")
+print(f"CT passed though sign_heaviside:\n{sign_heaviside(a, 0, 1, 0)}")
 

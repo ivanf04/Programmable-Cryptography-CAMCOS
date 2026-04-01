d68003ad65b186093cb642b4975eb4190c02d028
Isabel Faulds <isabel@bayou.energy>
2026-03-31 17:02:01 -0700

    equality function

---
+++ src/fhelib/auxiliary/equality.py
@@ -0,0 +1,14 @@
+from fhelib import Ciphertext
+from fhelib.lowlevel.sign import sign
+from fhelib.primitives.multiply import multiply
+import numpy as np
+
+def fhe_equality(ct: Ciphertext, a: float, epsilon: float = 0.01) -> Ciphertext:
+    """
+    Approximates (x == a) componentwise up to error epsilon.
+    Returns a 0-1 vector where 1 means the element is within epsilon of a.
+    Uses sigmoid-based sign approximation.
+    """
+    lower = sign(ct - (a - epsilon))   # x >= a - epsilon
+    upper = sign((a + epsilon) - ct)   # x <= a + epsilon
+    return multiply(lower, upper)
